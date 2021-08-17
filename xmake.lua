2ed9beea8f66dad9a7a122f01555f6f8f8769082
Kugge <sofiane.djerbi38@gmail.com>
2021-08-17 20:01:38 +0200

    xmake target and dependencies

---
+++ xmake.lua
@@ -0,0 +1,15 @@
+-- set xmake minimal version
+set_xmakever("2.3.8")
+
+-- set repository description
+set_description("The official package repository of xmake")
+
+
+-- dependencies
+add_requires("libsdl")
+
+-- target
+target("debug")
+    set_kind("binary")
+    add_files("src/*.c")
+    add_packages("libsdl")
